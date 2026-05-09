34c856ef96689eee952d1e12caaf0f0947366037
jongmin <bergi4025@gmail.com>
2026-05-10 02:12:04 +0900

    탭 라우팅 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,5 @@
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function HomeScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <View className="rounded-xl bg-black px-5 py-4">
-        <Text className="text-2xl font-bold text-red-500">
-          Tailwind is working
-        </Text>
-      </View>
-    </View>
-  );
+export default function TabsIndex() {
+  return <Redirect href="/main" />;
 }
---
+++ app/(tabs)/main/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function MainScreen() {
+  return (
+    <View>
+      <Text>메인 페이지</Text>
+    </View>
+  );
+}
